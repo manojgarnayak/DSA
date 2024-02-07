ed5c5670c96eb2b5836aff7ffd0a3a456b9bedab
manojgarnayak <manojgarnayak07@gmail.com>
2024-02-07 16:30:26 +0530

    Insersion of element

---
+++ Arrays/1D_Array/insersion.py
@@ -0,0 +1,10 @@
+import array
+
+myarray1 = array.array('i',[1,2,3,4])
+print(myarray1)
+# myarray1.insert(0,6)
+# print(myarray1)
+# myarray1.insert(2,6)
+# print(myarray1)
+myarray1.insert(4,6)
+print(myarray1)
